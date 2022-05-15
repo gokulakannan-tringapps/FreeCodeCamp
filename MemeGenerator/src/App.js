b99efbc0aef3a805301943ca3c7a8d0e107a8dc4
Gokula Kannan L <96760784+gokulakannan-tringapps@users.noreply.github.com>
2022-05-16 02:12:11 +0530

    Add files via upload

---
+++ MemeGenerator/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HeaderSection from './component/Header';
+import MemeForm from './component/MemeForm';
+function App() {
+  return (
+    <div className="App">
+		<HeaderSection />
+		<MemeForm />
+    </div>
+  );
+}
+
+export default App;
