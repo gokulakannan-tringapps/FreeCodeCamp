162d5715f362debba55655b0c8872edb292b1310
Gokula Kannan L <96760784+gokulakannan-tringapps@users.noreply.github.com>
2022-05-15 19:20:38 +0530

    Add files via upload

---
+++ BusinessCardT2/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import BCard from './component/BusinessCard';
+function App() {
+  return (
+    <div className="App">
+      <BCard/>
+    </div>
+  );
+}
+
+export default App;
