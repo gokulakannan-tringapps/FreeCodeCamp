85a82faa17e0a06460eb126f01b22693728990a4
Gokula Kannan L <96760784+gokulakannan-tringapps@users.noreply.github.com>
2022-05-17 14:38:05 +0530

    Add files via upload

---
+++ Travel_Journal/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Headerbar'
+import TravelCard from './components/TravelCard'
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <TravelCard/>
+    </div>
+  );
+}
+
+export default App;
